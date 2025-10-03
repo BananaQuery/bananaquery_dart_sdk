66dc5ca20ecffbebdf9176b3ef0e2487928dbdbc
Daniel Rodriguez <danielrodriguez@mac.lan>
2025-10-03 14:15:52 -0700

    use the latest colonel package for commands

--- packages/banana_query_core/lib/commands/food_plan_command.dart
+++ packages/banana_query_core/lib/commands/food_plan_command.dart
@@ -1,40 +1,11 @@
 import 'dart:async';
+import 'package:colonel/command_base.dart';
+
 import 'command_exceptions.dart';
 
 ///
 /// The PlanCommand is the base class for all
 ///
-enum ExecutionState { notExecuted, executed, undone, redone }
-
-abstract class BananaQueryCommand  {
-  ExecutionState executionState = ExecutionState.notExecuted;
-  static StreamController listener = StreamController();
-  bool notifyListenersOnExecution = true;
-
-  void assertCanExecute() {
-    if (executionState == ExecutionState.executed || executionState == ExecutionState.redone) {
-      throw CommandExecutionException();
-    }
-  }
-
-  void assertCanRedo() {
-    if (executionState != ExecutionState.undone) {
-      throw CommandRedoException();
-    }
-  }
-
-  void assertCanUndo() {
-    if (isUndoable == false) {
-      throw CommandUndoException();
-    }
-    if (executionState != ExecutionState.executed || executionState != ExecutionState.redone) {
-      throw CommandUndoException();
-    }
-  }
-
-
-  Future<bool> execute();
-  Future<bool> undo();
-  Future<bool> redo();
-  bool get isUndoable;
+abstract class BananaQueryCommand  extends CommandBase {
+ 
 }
